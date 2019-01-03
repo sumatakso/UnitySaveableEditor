d64e06f8756e36865d739e94a9f0dfc87f99731d
Sukri Preangthong <sumataksounit@gmail.com>
2019-01-04 03:32:16 +0700

    Inital commit

---
+++ Assets/SavableEditor/Editor/SaveableEditor.cs
@@ -0,0 +1,118 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class SaveableEditor
+{
+    #region Private var and method
+    public static string SaveFolder
+    {
+        get { return Path.Combine(Application.dataPath, ".." + Path.DirectorySeparatorChar + "SaveableEditorData"); }
+    }
+    const string DEFAULT_SECTION = "MAIN";
+    const string FILENAME_FORMAT = "{0}-{1}.json";
+    public static string ToFilePath(string editorName, string valueSection)
+    {
+        return Path.Combine(SaveFolder,string.Format(FILENAME_FORMAT,editorName, valueSection));
+    }
+    public static void SaveTo(string editorName, string section, string jsonString)
+    {
+        string useSection = string.IsNullOrEmpty(section) ? DEFAULT_SECTION : section;
+        string filePath = ToFilePath(editorName, useSection);
+        if (!Directory.Exists(SaveFolder))
+        {
+            Directory.CreateDirectory(SaveFolder);
+        }
+        File.WriteAllText(filePath, jsonString);
+    }
+    #endregion
+
+    #region Public method
+    /// <summary>
+    /// Save editor property class
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="editorName"></param>
+    /// <param name="saveValue"></param>
+    public static void Save<T>(string editorName, T saveValue)
+    {
+        Save(editorName, DEFAULT_SECTION, saveValue);
+    }
+    /// <summary>
+    /// Save editor property class
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="editorName"></param>
+    /// <param name="section"></param>
+    /// <param name="saveValue"></param>
+    public static void Save<T>(string editorName, string section, T saveValue)
+    {
+        SaveTo(editorName, section, JsonUtility.ToJson(saveValue, true));
+    }
+    /// <summary>
+    /// Load Editor property class by provide default if it's not there.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="editorName"></param>
+    /// <param name="defaultValue"></param>
+    /// <returns></returns>
+    public static T Load<T>(string editorName, T defaultValue)
+    {
+        return Load(editorName, DEFAULT_SECTION, defaultValue);
+    }
+    /// <summary>
+    /// Load Editor property class by provide default if it's not there.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="editorName"></param>
+    /// <param name="section"></param>
+    /// <param name="defaultValue"></param>
+    /// <returns></returns>
+    public static T Load<T>(string editorName, string section, T defaultValue)
+    {
+        string filePath = ToFilePath(editorName, section);
+        if (File.Exists(filePath))
+        {
+            return JsonUtility.FromJson<T>(File.ReadAllText(filePath));
+        }
+        return defaultValue;
+    }
+    /// <summary>
+    /// Remove save for editor, send none section will result in all save will be remove.
+    /// </summary>
+    /// <param name="editorName"></param>
+    /// <param name="sections"></param>
+    public static void Remove(string editorName, params string[] sections)
+    {
+        if (!Directory.Exists(SaveFolder))
+        {
+            return;
+        }
+        if (sections.Length == 0)
+        {
+            foreach(string path in Directory.EnumerateFiles(SaveFolder))
+            {
+                if (Path.GetFileName(path).StartsWith(editorName))
+                {
+                    File.Delete(path);
+                }
+            }
+        }
+        else
+        {
+            foreach (string section in sections)
+            {
+                string path = ToFilePath(editorName, section);
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+                }
+            }
+        }
+        string[] files = Directory.GetFiles(SaveFolder);
+        if (files.Length == 0)
+        {
+            Directory.Delete(SaveFolder);
+        }
+    }
+    #endregion
+}
---
+++ Assets/SavableEditor/Editor/TestEditorSaveWindow.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class TestEditorSaveWindow : EditorWindow
+{
+    public struct SaveEditorData
+    {
+        public string someString;
+        public bool isToggle;
+    }
+
+    SaveEditorData saveEditor = new SaveEditorData
+    {
+        someString = "Some string to save",
+        isToggle = false
+    };
+    string saveSection = "MAIN";
+
+    [MenuItem("Savable Example/Window")]
+    static void OpenWindow()
+    {
+        GetWindow<TestEditorSaveWindow>();
+    }
+    private void OnEnable()
+    {
+        saveEditor = SaveableEditor.Load(this.GetType().Name, saveSection, saveEditor);
+    }
+    private void DrawPropertyArea()
+    {
+        EditorGUILayout.LabelField("Save properties", EditorStyles.boldLabel);
+        EditorGUILayout.BeginVertical("Box");
+        saveEditor.someString = EditorGUILayout.TextField("Some string", saveEditor.someString);
+        saveEditor.isToggle = EditorGUILayout.Toggle("Toggle", saveEditor.isToggle);
+        EditorGUILayout.EndHorizontal();
+    }
+    private void DrawFunctionArea()
+    {
+        EditorGUILayout.LabelField("Function", EditorStyles.boldLabel);
+        saveSection = EditorGUILayout.TextField("Save section", saveSection);
+        EditorGUILayout.BeginHorizontal();
+        if (GUILayout.Button("Save"))
+        {
+            SaveableEditor.Save(this.GetType().Name, saveSection, this.saveEditor);
+        }
+        if (GUILayout.Button("Load"))
+        {
+            this.saveEditor = SaveableEditor.Load(this.GetType().Name, saveSection, this.saveEditor);
+        }
+        if (GUILayout.Button("Remove"))
+        {
+            SaveableEditor.Remove(this.GetType().Name, saveSection);
+        }
+        if (GUILayout.Button("Remove All"))
+        {
+            SaveableEditor.Remove(this.GetType().Name);
+        }
+        EditorGUILayout.EndHorizontal();
+    }
+    private void OnGUI()
+    {
+        this.DrawPropertyArea();
+        EditorGUILayout.Space();
+        this.DrawFunctionArea();
+    }
+}
